7a68a973d17d489f19af9166b7c1927f1d84e35e
sachinlalms <sachinlalms95@gmail.com>
2021-11-11 21:35:26 +0530

    function_completed

--- sample_js/function.js
+++ sample_js/function.js
@@ -1,6 +1,24 @@
+// No Arguments no return value
 function hello(){
     console.log('Hello World')
 }
-hello();
 
-function hi()
+//Arguments not return values
+function sum(num1, num2){
+    console.log(num1+num2)
+}
+//Arguments with return value
+function sum2(num1,num2 ){
+    return num1+num2;
+}
+console.log(sum2(8,2))
+//No arguments bt return value
+function sum3(){
+    num1=9;
+    num2=99;
+    return num1+num2;
+}
+hello()
+sum(5,9)
+var result=sum3()
+console.log(result)
